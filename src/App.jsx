ff8d2da134ef22044ec5464ec24cc6225a0acfc1
SimranKumbhare <simrankumbhare19@gmail.com>
2025-10-17 09:25:39 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import ProgressTracker from "./Components/ProgressTracker";
 import TaskForm from "./Components/TaskForm";
 import TaskList from "./Components/TaskList";
 
 export default function App() {
+
+  const[tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  })
+
+  const addTask = (task) => {
+    setTasks([...tasks,task]
+      
+    );
+  }
   return(
     <div>
       <h1>TaskNest</h1>
       <h2>Your tasks, neatly nested.</h2>
-      <TaskForm />
+      <TaskForm addTask= {addTask}/>
       <TaskList />
       <ProgressTracker />
       <button>Clear All Tasks</button>
--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -1,16 +1,34 @@
-export default function TaskForm() {
+import { useState } from "react"
+
+export default function TaskForm({addTask}) {
+    const[task, setTask] = useState('');
+    const[priority, setPriority] = useState('Medium');
+    const[type, setType] = useState('General');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTask({text: task, priority, type, completed: false});
+
+        //reset
+        setTask('');
+        setPriority('Medium');
+        setType('General');
+    }
+
     return(
-        <form>
-            <input type="text" placeholder="Add a new task..." />
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Add a new task..." value={task}
+            onChange={(e) => setTask(e.target.value)}/>
+            {/* <h1>{task} {priority} {type}</h1> */}
             <div>
-                <select>
+                <select value={priority} onChange={(e) => setPriority(e.target.value)}>
                     <option value="" disabled selected>Select task Priority</option>
                     <option value="high">High</option>
                     <option value="medium">Medium</option>
                     <option value="low">Low</option>
                 </select>
                 
-                <select name="" id="">
+                <select value={type} onChange={(e)=> setType(e.target.value)}>
                     <option value="" disabled selected> Select type of task </option>
                     <option value="general">General</option>
                     <option value="work">Work</option>
